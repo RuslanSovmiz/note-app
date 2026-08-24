2d2e3534e6db961794f807eac74c2c2a03ae6a72
RuslanSovmiz <sovmiz98@mail.ru>
2026-08-24 21:06:22 +0300

    Первый коммит: приложение для заметок

---
+++ src/Note.java
@@ -0,0 +1,67 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
+
+public class Note {
+
+    private static int counter = 1;
+    private int id;
+    private String title;
+    private String content;
+    private String createdAt;
+    private String updatedAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public void updateUpdatedAt() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        updatedAt = LocalDateTime.now().format(formatter);
+    }
+
+    public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        createdAt = LocalDateTime.now().format(formatter);
+        this.id = counter;
+        counter++;
+        updateUpdatedAt();
+    }
+
+
+
+
+
+
+}
---
+++ src/NoteApp.java
@@ -0,0 +1,174 @@
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class NoteApp {
+    Scanner scanner = new Scanner(System.in);
+    HashMap<Integer, Note> notes = new HashMap<>();
+
+    public static void main(String[] args) {
+        NoteApp app = new NoteApp();
+        app.runMenu();
+
+
+        }
+
+
+
+    public void addNote() {
+        System.out.println("Введите заголовок: ");
+        String title = scanner.nextLine();
+        System.out.println("Введите текст заметки: ");
+        String text = scanner.nextLine();
+        if (title.trim().isEmpty()) {
+            System.out.println("Ошибка: заголовок не может быть пустым!");
+            return;
+        }
+        if (text.trim().isEmpty()) {
+            System.out.println("Ошибка: заметка не может быть пустой!");
+            return;
+        } else {
+            Note newNote = new Note(title, text);
+            notes.put(newNote.getId(), newNote);
+            System.out.println("Заметка успешно создана!");
+            System.out.println("ID: " + newNote.getId());
+        }
+
+    }
+
+    public void showAllNotes() {
+        if (notes.isEmpty()) {
+            System.out.println("Заметок пока нет!");
+        } else {
+            for (Note note : notes.values()) {
+                System.out.println("ID: " + note.getId() + "\nЗаголовок: " + note.getTitle() +
+                        "\nДата создания: " + note.getCreatedAt() + "\n" + note.getContent() + "\n-----------\n");
+            }
+        }
+    }
+
+    public void editNote() {
+        while (true) {
+            System.out.println("Введите ID заметки: ");
+            try {
+                int findId = scanner.nextInt();
+                scanner.nextLine();
+                Note foundNote = notes.get(findId);
+                if (foundNote == null) {
+                    System.out.println("Заметка с таким ID не найдена!");
+                    return;
+                } else if (foundNote != null) {
+                    System.out.println("Введите новый заголовок (Enter - оставить без изменений): ");
+                    String newTitle = scanner.nextLine();
+                    if (!newTitle.trim().isEmpty()) {
+                        foundNote.setTitle(newTitle);
+                    }
+                    System.out.println("Введите новый текст (Enter - оставить без изменений): ");
+                    String newContent = scanner.nextLine();
+                    if (!newContent.trim().isEmpty()) {
+                        foundNote.setContent(newContent);
+                    }
+                    foundNote.updateUpdatedAt();
+                    System.out.println("Заметка успешно обновлена!");
+                    break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Введите число!");
+                scanner.nextLine();
+            }
+        }
+
+    }
+
+    public void deleteNote() {
+        while (true) {
+            System.out.println("Введите ID заметки: ");
+            try {
+                int findId = scanner.nextInt();
+                scanner.nextLine();
+                Note foundNote = notes.get(findId);
+                if (foundNote == null) {
+                    System.out.println("Заметка с таким ID не найдена!");
+                    return;
+                } else {
+                    notes.remove(findId);
+                    System.out.println("Заметка успешно удалена!");
+                    return;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Введите число!");
+                scanner.nextLine();
+            }
+        }
+    }
+
+    public void showNoteById() {
+
+        while (true) {
+            System.out.print("Введите ID заметки: ");
+            try {
+                int findId = scanner.nextInt();
+                scanner.nextLine();
+                Note foundNote = notes.get(findId);
+                if (foundNote !=  null) {
+                    System.out.println("ID: " + foundNote.getId());
+                    System.out.println("Заголовок : " + foundNote.getTitle());
+                    System.out.println("Текст: " + foundNote.getContent());
+                    System.out.println("Дата создания: " + foundNote.getCreatedAt());
+                    break;
+                } else {
+                    System.out.println("Заметка с таким ID не найдена!");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Введите число!");
+                scanner.nextLine();
+            }
+
+        }
+    }
+
+    public void runMenu() {
+        while (true) {
+            System.out.println("1. Добавить заметку");
+            System.out.println("2. Показать все заметки");
+            System.out.println("3. Показать заметку по ID");
+            System.out.println("4. Редактировать заметку");
+            System.out.println("5. Удалить заметку");
+            System.out.println("0. Выход");
+            System.out.print("Выберите действие: ");
+            try {
+
+                int choise = scanner.nextInt();
+                scanner.nextLine();
+                switch (choise) {
+                    case 1:
+                        addNote();
+                        break;
+                    case 2:
+                        showAllNotes();
+                        break;
+                    case 3:
+                        showNoteById();
+                        break;
+                    case 4:
+                        editNote();
+                        break;
+                    case 5:
+                        deleteNote();
+                        break;
+                    case 0:
+                        return;
+                    default:
+                        System.out.println("Неверный выбор!");
+
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Введите число!");
+                scanner.nextLine();
+            }
+
+}
+
+}
+}
+
